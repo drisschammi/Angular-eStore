0f20b46912bcf1d7104c797b10cb3ccfa43d2864
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-10 08:18:41 +0100

    refactor(home): move header and category navigation as child components

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { CatnavigationComponent } from './catnavigation/catnavigation.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, CatnavigationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

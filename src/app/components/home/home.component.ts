6b94b8e6863fb7f05f1c1cb8ecf69715d5e46d52
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-10 09:37:15 +0100

    feat(side-navigation): create accordion-based side navigation component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { CatnavigationComponent } from './catnavigation/catnavigation.component';
+import { SidenavigationComponent } from "./sidenavigation/sidenavigation.component";
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, CatnavigationComponent],
+  imports: [HeaderComponent, CatnavigationComponent, SidenavigationComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

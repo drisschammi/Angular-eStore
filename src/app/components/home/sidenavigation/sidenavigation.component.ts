19e3b589742e750e1802fa361f60e9723dad5f70
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-10 10:08:40 +0100

    feat(side-navigation): fetch and display categories using service

--- src/app/components/home/sidenavigation/sidenavigation.component.ts
+++ src/app/components/home/sidenavigation/sidenavigation.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { Category } from '../types/category';
+import { CategoryService } from '../services/category.service';
 
 @Component({
   selector: 'app-sidenavigation',
   imports: [FontAwesomeModule],
   templateUrl: './sidenavigation.component.html',
-  styleUrl: './sidenavigation.component.css'
+  styleUrl: './sidenavigation.component.css',
 })
 export class SidenavigationComponent {
   faAngleDown = faAngleDown;
+
+  categories: Category[] = [];
+
+  constructor(categoryService: CategoryService) {
+    this.categories = categoryService.getAllCategories();
+  }
+
+  getCategories(parentCategory?: number): Category[] {
+    return this.categories.filter(
+      (c) => c.parent_category_id === parentCategory
+    );
+  }
 }

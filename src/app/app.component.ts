219b5593d9f92145fb53d3feeb293c9ede628733
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-10 08:13:07 +0100

    feat(home): create home component and configure default routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./components/header/header.component";
-import { CatnavigationComponent } from "./components/catnavigation/catnavigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, CatnavigationComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'store';
 }

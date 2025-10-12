bb03589805cd731be3be4c916f3b29df20dcaf75
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-12 12:31:09 +0100

    feat(server): establish MySQL database connection using connection pool and environment variables
    - Moved database credentials (host, user, password, database, port) to .env file
    
    - Configured dotenv to load environment variables in index.js

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,29 @@
+require("dotenv").config();
 const express = require("express");
+const mysql = require("mysql2");
+
 const app = express();
-const PORT = 5001;
+
+const PORT = process.env.PORT || 5001;
+
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  multipleStatements: true,
+});
+
+app.get("/", (req, res) => {
+  pool.getConnection((err, connection) => {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.status(200).send("Connection Established");
+    }
+  });
+});
 
 const server = app.listen(PORT, () => {
   console.log("App is running on the port - 5001");

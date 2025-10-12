9fe165fe15b4dd544ec6bb5a2130c2a4bf4b85f3
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-12 16:55:21 +0100

    feat(cat-navigation): fetch and display top-level categories from server

--- frontend/src/app/components/home/catnavigation/catnavigation.component.ts
+++ frontend/src/app/components/home/catnavigation/catnavigation.component.ts
@@ -1,11 +1,24 @@
 import { Component } from '@angular/core';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../types/category';
 
 @Component({
   selector: 'app-catnavigation',
   imports: [],
   templateUrl: './catnavigation.component.html',
-  styleUrl: './catnavigation.component.css'
+  styleUrl: './catnavigation.component.css',
 })
 export class CatnavigationComponent {
+  categories: Category[] = [];
 
+  constructor(categoryService: CategoryService) {
+    categoryService
+      .getAllCategories()
+      .subscribe(
+        (categories) =>
+          (this.categories = categories.filter(
+            (category) => category.parent_category_id === null
+          ))
+      );
+  }
 }

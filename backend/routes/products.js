41b32d295270e9fdd8b8f964264b10f56d2d1218
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-13 20:32:19 +0100

    feat(server): add query parameters for category and subcategory filtering

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -3,7 +3,21 @@ const router = express.Router();
 const pool = require("../shared/pool");
 
 router.get("/", (req, res) => {
-  pool.query("select * from products", (error, products) => {
+  const mainCategoryId = req.query.maincategoryid;
+  const subCategoryId = req.query.subcategoryid;
+
+  let query = "select * from products";
+  let queryParams = [];
+
+  if (mainCategoryId) {
+    query = `select products.* from products join categories on products.category_id = categories.id where categories.parent_category_id = ?`;
+    queryParams.push(mainCategoryId);
+  } else if (subCategoryId) {
+    query += " where category_id = ?";
+    queryParams.push(subCategoryId);
+  }
+
+  pool.query(query, queryParams, (error, products) => {
     if (error) res.status(500).send(error);
     else res.status(200).send(products);
   });

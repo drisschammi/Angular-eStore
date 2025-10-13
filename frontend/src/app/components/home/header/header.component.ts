038a30d2c457675059610e615579ddf19cea4bdd
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-13 19:45:43 +0100

    feat(header): populate search dropdown categories from store

--- frontend/src/app/components/home/header/header.component.ts
+++ frontend/src/app/components/home/header/header.component.ts
@@ -1,16 +1,22 @@
 import { Component } from '@angular/core';
-import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
-import {faSearch, faUserCircle, faHeart, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {
+  faSearch,
+  faUserCircle,
+  faShoppingCart,
+} from '@fortawesome/free-solid-svg-icons';
+import { CategoriesStoreItem } from '../services/categories.storeItem';
 
 @Component({
   selector: 'app-header',
   imports: [FontAwesomeModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   faSearch = faSearch;
   faUserCircle = faUserCircle;
-  faHeart = faHeart;
   faShoppingCart = faShoppingCart;
+
+  constructor(public categoryStore: CategoriesStoreItem) {}
 }

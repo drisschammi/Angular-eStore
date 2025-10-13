fb663e452ad09e539397a6c780e2572fb6762f28
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-13 20:06:49 +0100

    feat(products): fetch products from server API on client side

--- frontend/src/app/components/products/products.component.ts
+++ frontend/src/app/components/products/products.component.ts
@@ -1,22 +1,22 @@
 import { Component } from '@angular/core';
 import { ProductsService } from './products.service';
-import { ProductListItem } from './products.type';
+import { Product } from './products.type';
 import { CommonModule } from '@angular/common';
-import { RatingsComponent } from "../ratings/ratings.component";
+import { RatingsComponent } from '../ratings/ratings.component';
 
 @Component({
   selector: 'app-products',
   imports: [CommonModule, RatingsComponent],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
-  providers: [ ProductsService ],
+  providers: [ProductsService],
 })
 export class ProductsComponent {
-
-  products: ProductListItem[] = [];
+  products: Product[] = [];
 
   constructor(private productsService: ProductsService) {
-    this.products = this.productsService.getProductsList();
+    productsService.getAllProducts().subscribe((data) => {
+      this.products = data;
+    });
   }
-
 }

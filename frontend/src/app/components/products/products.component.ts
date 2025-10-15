2936d09635130f513810e75140c68e320507d674
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-15 22:03:55 +0100

    feat(products): display no products found message with icon

--- frontend/src/app/components/products/products.component.ts
+++ frontend/src/app/components/products/products.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { Product } from '../home/types/products.type';
 import { CommonModule } from '@angular/common';
 import { RatingsComponent } from '../ratings/ratings.component';
 import { ProductsService } from '../home/services/product/products.service';
 import { ProductsStoreItem } from '../home/services/product/products.storeItem';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faBoxOpen } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-products',
-  imports: [CommonModule, RatingsComponent],
+  imports: [CommonModule, RatingsComponent, FontAwesomeModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
   providers: [ProductsService],
 })
 export class ProductsComponent {
+
+  faBoxOpen = faBoxOpen;
 
   constructor(public productsStoreItem: ProductsStoreItem) {}
   

d86b0c527363e5bf51a6affdf36629ac096ac374
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-22 19:25:35 +0100

    feat(server): create user signup API with bcrypt password hashing

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,30 @@
+// Environment Configuration
 require("dotenv").config();
+
+// Module Imports
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// Route Imports
 const productCategories = require("./routes/productCategories");
 const products = require("./routes/products");
+const user = require("./routes/users");
 
-const cors = require("cors");
+// App Initialization
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// Middleware
 app.use(cors());
+app.use(bodyParser.json());
 
+// Routes
 app.use("/productCategories", productCategories);
 app.use("/products", products);
+app.use("/users", user);
 
+// Server Setup
 const server = app.listen(PORT, () => {
-  console.log("App is running on the port - 5001");
+  console.log(`App is running on the port - ${PORT}`);
 });
---
+++ backend/routes/users.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const pool = require("../shared/pool");
+const bcrypt = require("bcryptjs");
+
+const user = express.Router();
+
+user.post("/signup", async (req, res) => {
+  const { firstName, lastName, address, city, state, pin, email, password } =
+    req.body;
+
+  try {
+    const [existingUser] = await pool
+      .promise()
+      .query("select count(*) as count from users where email = ?", [email]);
+    if (existingUser[0].count > 0) {
+      return res.status(200).send("Email already exists");
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await pool
+      .promise()
+      .query(
+        `insert into users (email, firstName, lastName, address, city, state, pin, password) values (?, ?, ?, ?, ?, ?, ?, ?)`,
+        [email, firstName, lastName, address, city, state, pin, hashedPassword]
+      );
+
+    res.status(201).send("Success");
+  } catch (error) {
+    res.status(500).send(error.message) || "Something went wrong";
+  }
+});
+
+module.exports = user;

cedc0d259473ac2f957d5cd8a0d3b4d46527d977
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-22 18:02:36 +0100

    feat(product-details): add 'add to cart' button with cart functionality

--- frontend/src/app/components/home/product-details/product-details.component.ts
+++ frontend/src/app/components/home/product-details/product-details.component.ts
@@ -5,18 +5,23 @@ import { ProductsService } from '../services/product/products.service';
 import { Product } from '../types/products.type';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { CommonModule } from '@angular/common';
+import { CartStoreItem } from '../services/cart/cart.storeItem';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-product-details',
-  imports: [RatingsComponent, CommonModule],
+  imports: [RatingsComponent, CommonModule, FontAwesomeModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css',
 })
 export class ProductDetailsComponent {
   private readonly route = inject(ActivatedRoute);
   private readonly productsService = inject(ProductsService);
+  private readonly cart = inject(CartStoreItem);
 
   readonly product = signal<Product | null>(null);
+  faShoppingCart = faShoppingCart;
 
   constructor() {
     const idParam = this.route.snapshot.paramMap.get('id');
@@ -32,4 +37,12 @@ export class ProductDetailsComponent {
       return;
     }
   }
+
+  addToCart() {
+    const product = this.product();
+    if (product) {
+      this.cart.addProduct(product);
+    }
+  }
+
 }

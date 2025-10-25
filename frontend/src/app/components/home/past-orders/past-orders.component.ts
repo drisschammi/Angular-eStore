feb7eb2f966d3b1ff3adf4678269c6f7a407ba3f
driss-chammi <driss.chammi@usmba.ac.ma>
2025-10-25 21:03:02 +0100

    feat(past-orders): design template to display past ordered products

--- frontend/src/app/components/home/past-orders/past-orders.component.ts
+++ frontend/src/app/components/home/past-orders/past-orders.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { PastOrder, PastOrderProduct } from '../types/order.type';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-past-orders',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './past-orders.component.html',
-  styleUrl: './past-orders.component.css'
+  styleUrl: './past-orders.component.css',
 })
 export class PastOrdersComponent {
+  pastOrderProducts: PastOrderProduct[] = [
+    {
+      amount: 100,
+      price: 50,
+      productId: 1,
+      productImage: 'shop-1.jpg',
+      productName: 'Jacket',
+      qty: 2,
+    },
+  ];
 
+  pastOrder: PastOrder = {
+    address: 'Address goes here',
+    city: 'New Jersey',
+    orderDate: '03/01/23',
+    pin: '12345',
+    state: 'NY',
+    total: 100,
+    userName: 'Thomas Brown',
+  };
 }
